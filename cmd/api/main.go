21fff432d7191dead6aa08b7d6cbe5d596c49192
Adnan <adbak.dev@gmail.com>
2024-09-29 20:59:37 +0200

    store

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,18 @@ import (
 	"log"
 
 	"github.com/ad-bak/chi/internal/env"
+	"github.com/ad-bak/chi/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("API_ADDR", ":8080"),
 	}
+	store := store.NewPostgresStorage(nil)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
